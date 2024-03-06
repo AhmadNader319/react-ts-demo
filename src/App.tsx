35a4a795e6e37c54f9e312a68f2a270177e1f192
AhmadNader319 <anader3194747@gmail.com>
2024-03-06 16:31:44 +0200

    clean code, start new demo branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,9 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Status } from './components/Status';
-import { Person } from './components/person';
-import { NameList } from './components/personList'; 
-import { Heading }from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { PostList } from './components/PostList';
 
 function App() {
-  const personName = {
-    first: 'Ahmad',
-    last: 'Nader',
-  };
-
-  const namesList = [
-    {
-      first: 'Hana',
-      last: 'Ahmad',
-    },
-    {
-      first: 'Nader',
-      last: 'AbdelMoneim',
-    },
-    {
-      first: 'Sayed',
-      last: 'Ibrahim',
-    },
-  ];
-
+  
   return (
-    <div className="App">
-      <Greet name="ahmed" isLogged={true} />
-      <Person name={personName} />
-      <NameList names={namesList} /> {/* Update the component name and pass the prop */}
-      <Status status='success' />
-      <Oscar>
-           <Heading>Ahmad goes to His daughter</Heading>
-      </Oscar>
-      <PostList />
-    </div>
-    
+    <div>new demo</div>
   );
 }
 
--- src/components/Heading.tsx
+++
@@ -1,8 +0,0 @@
-// This component represents a heading, utilizing the heading tag and allowing for a placeholder to be inserted.
-type HeadingProps = {
-    children: string;
-};
-
-export const Heading = (props: HeadingProps) => {
-    return <h2>{props.children}</h2>;
-};
